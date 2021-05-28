0aec9cf321e7014af2defe39843353b85b71fe34
Zack Katz <zack@katz.co>
2021-05-28 18:05:14 -0400

    Remove deprecated code

--- src/assets/trustedlogin.js
+++ src/assets/trustedlogin.js
@@ -5,102 +5,6 @@
 
 	$( document ).ready( function () {
 
-		/**
-		 * TODO: Deprecate
-		 **/
-		function outputErrorAlert( response, tl_obj ) {
-
-			var settings = {
-				icon: 'dashicons dashicons-no',
-				title: tl_obj.lang.status.failed.title,
-				content: tl_obj.lang.status.failed.content + '<pre>' + JSON.stringify( response ) + '</pre>',
-				escapeKey: 'ok',
-				type: 'red',
-				theme: 'material',
-				buttons: {
-					ok: {
-						text: tl_obj.lang.buttons.ok
-					}
-				}
-			};
-
-			switch ( response.status ) {
-
-				case 404: /** vendor not found */
-				settings.title = tl_obj.lang.status.error404.title;
-					settings.content = tl_obj.lang.status.error404.content;
-					break;
-
-				case 409: /** user already exists */
-				settings.title = tl_obj.lang.status.error409.title;
-					settings.content = tl_obj.lang.status.error409.content;
-					break;
-
-				case 500: /** problems syncing to SaaS */
-				case 501:
-				case 502:
-				case 503:
-				case 504:
-				case 522:
-					settings.title = tl_obj.lang.status.error.title;
-					settings.content = tl_obj.lang.status.error.content;
-					settings.icon = 'dashicons dashicons-external';
-					settings.escapeKey = 'close';
-					settings.type = 'orange';
-					settings.buttons = {
-						goToSupport: {
-							text: tl_obj.lang.buttons.go_to_site,
-							action: function ( goToSupportButton ) {
-								window.open( tl_obj.vendor.support_url, '_blank' );
-								return false; // you shall not pass
-							},
-						},
-						close: {
-							text: tl_obj.lang.buttons.close
-						},
-					};
-					break;
-			}
-
-			$.alert( settings );
-		}
-
-		/**
-		 * TODO: Deprecate
-		 **/
-		function outputAccessKey( accessKey, tl_obj ) {
-
-			var settings = {
-				icon: 'dashicons dashicons-yes',
-				title: tl_obj.lang.status.accesskey.title,
-				content: tl_obj.lang.status.accesskey.content + '<pre>' + accessKey + '</pre>',
-				escapeKey: 'close',
-				type: 'green',
-				theme: 'material',
-				buttons: {
-					goToSupport: {
-						text: tl_obj.lang.buttons.go_to_site,
-						action: function ( goToSupportButton ) {
-							window.open( tl_obj.vendor.support_url, '_blank' );
-							return false; // you shall not pass
-						},
-						btnClass: 'btn-blue',
-					},
-					revokeAccess: {
-						text: tl_obj.lang.buttons.revoke,
-						action: function ( revokeAccessButton ){
-							window.location.assign( tl_obj.lang.status.accesskey.revoke_link );
-						},
-					},
-					close: {
-						text: tl_obj.lang.buttons.close
-					}
-				}
-			};
-
-			$.alert( settings );
-		}
-
 		function outputStatus( content, type ){
 
 			var dialogClass = 'tl-' + tl_obj.vendor.namespace + '-auth';
@@ -116,9 +20,9 @@
 			}
 
 			// Reset the class and set the type for contextual styling.
-			$responseDiv.attr('class', responseClass).addClass('tl-'+ tl_obj.vendor.namespace + '-auth__response_' + type );
-			$responseDiv.text( content );
-
+			$responseDiv
+				.attr('class', responseClass).addClass('tl-'+ tl_obj.vendor.namespace + '-auth__response_' + type )
+				.text( content );
 
 			/**
 			 * Handle buttong actions/labels/etc to it's own function
@@ -173,15 +77,9 @@
 						/**
 						 * TODO: Will be replaced with error message
 						 **/
-						outputAccessKey( response.data.access_key, tl_obj );
+						//outputAccessKey( response.data.access_key, tl_obj );
 					}
 
-					/**
-					 * TODO: Removed as we no longer need the button to do popups
-					 **/
-					if ( response.data.access_key ){
-						$( tl_obj.selector ).data('accesskey', response.data.access_key );
-					}
 				} else if ( typeof response.data === 'object' ) {
 					outputStatus( tl_obj.lang.status.failed.content + ' ' + response.data.message, 'error' );
 				}
@@ -218,87 +116,15 @@
 
 		/**
 		 * TODO: Deprecate
-		 **/
-		function triggerLoginGeneration() {
-			var data = {
-				'action': 'tl_' + tl_obj.vendor.namespace + '_gen_support',
-				'vendor': tl_obj.vendor.namespace,
-				'_nonce': tl_obj._nonce,
-			};
-
-			if ( tl_obj.debug ) {
-				console.log( data );
-			}
-
-			$.post( tl_obj.ajaxurl, data, function ( response ) {
-
-				if ( tl_obj.debug ) {
-					console.log( response );
-				}
-
-				if ( response.success && typeof response.data == 'object' ) {
-
-					if ( response.data.is_ssl ){
-						$.alert( {
-							icon: 'dashicons dashicons-yes',
-							theme: 'material',
-							title: tl_obj.lang.status.synced.title,
-							type: 'green',
-							escapeKey: 'ok',
-							content: tl_obj.lang.status.synced.content,
-							buttons: {
-								ok: {
-									text: tl_obj.lang.buttons.ok
-								}
-							}
-						} );
-					} else {
-						outputAccessKey( response.data.access_key, tl_obj );
-					}
-
-					if ( response.data.access_key ){
-						$( tl_obj.selector ).data('accesskey', response.data.access_key );
-					}
-
-
-
-				} else {
-					outputErrorAlert( response, tl_obj );
-				}
-
-			} ).fail( function ( response ) {
-
-				outputErrorAlert( response, tl_obj );
-
-			} ).always( function( response ) {
-
-				if ( ! tl_obj.debug ) {
-					return;
-				}
-
-				if ( typeof response.data == 'object' ) {
-					console.log( 'TrustedLogin support login URL:' );
-					console.log( response.data.site_url + '/' + response.data.endpoint + '/' + response.data.identifier );
-				}
-			});
-		}
-
-		/**
-		 * TODO: Deprecate
 		 * No longer show alert.
 		 **/
 		$( 'body' ).on( 'click', tl_obj.selector, function ( e ) {
 
 			e.preventDefault();
 
-			if ( $( this ).data( 'accesskey' ) ){
-				outputAccessKey( $( this ).data( 'accesskey'), tl_obj );
-				return false;
-			}
-
 			grantAccess( $( this ) );
-			return false;
 
+			return false;
 		} );
 
 
