1c09564263bf20d156fec3516e5d03fa9c3d2b32
Josh Pollock <jpollock412@gmail.com>
2023-04-04 18:05:09 -0400

    Add the Form class Refactor out the form-generation from Admin.php to Form.php #111

---
+++ src/Form.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class Form
+ *
+ * @package TrustedLogin\Client
+ *
+ * @copyright 2021 Katz Web Services, Inc.
+ */
+namespace TrustedLogin;
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use \WP_User;
+
+
+/**
+ * Creates the TrustedLogin support user form.
+ * 	- Makes the HTML
+ *  - Manages assets
+ * Does not
+ * 	- Handle the form submission ./Ajax.php
+ *  - Setup the menu ./Admin.php
+ */
+final class Form {
+
+}
