5539c420fef2ddf4a93f4620f943295dca2cd113
Josh Pollock <jpollock412@gmail.com>
2023-04-05 13:17:17 -0400

    Refactor test plugin src/.wp-env.json is invalid. #114

--- client.php
+++ client.php
@@ -2,7 +2,26 @@
 /**
  * Plugin Name: TrustedLogin Client Test
  */
-
+/**
+ * Autoloader for the TrustedLogin Client
+ *
+ * @param string $class The fully-qualified class name.
+ * @see https://www.php-fig.org/psr/psr-4/examples/
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+    $prefix = 'TrustedLogin\\';
+    $base_dir = __DIR__ . '/src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 /**
  * Configuration for TrustedLogin Client
  *
@@ -25,33 +44,9 @@ $config = [
 ];
 
 try {
-    // Check class_exists() for sites running PHP 5.2.x
-    if ( class_exists( 'TrustedLogin\Client') ) {
-        new \TrustedLogin\Client(
-            new \TrustedLogin\Config( $config )
-        );
-    }
+	new \TrustedLogin\Client(
+		new \TrustedLogin\Config( $config )
+	);
 } catch ( \Exception $exception ) {
     error_log( $exception->getMessage() );
 }
-
-/**
- * Autoloader for the TrustedLogin Client
- *
- * @param string $class The fully-qualified class name.
- * @see https://www.php-fig.org/psr/psr-4/examples/
- * @return void
- */
-spl_autoload_register(function ($class) {
-    $prefix = 'TrustedLogin\\';
-    $base_dir = __DIR__ . '/src/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
