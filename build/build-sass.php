3a4314a0d1a510bd6fa218537a2a4b1a94a61a48
Zack Katz <zack@katz.co>
2024-02-14 11:27:49 -0500

    Attempt to get unit tests working…

--- build/build-sass.php
+++ build/build-sass.php
@@ -10,7 +10,7 @@
 $slug      = env( 'TRUSTEDLOGIN_SLUG' );
 $file_name = $slug . '.scss';
 
-$path  = rtrim( dirname( __DIR__ ), '/\\' ); // Path to trustedlogin-client/, untrailingslashit()
+$path  = rtrim( dirname( __DIR__ ), '/\\' ); // Path to trustedlogin-client/, untrailingslashit().
 $path .= '/src/assets/src/';
 
 file_put_contents( $path . $file_name, '$namespace: "' . $slug . '" !default;' );
